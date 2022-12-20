24e6e83db4f715057ffdff0b347031676ad6ccf0
bcsyzwqhz <makerLai@163.com>
2022-12-20 17:56:59 +0800

    乱码V1

---
+++ 乱码.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	while(1)
+	{
+	
+	char ch;
+	if(rand()%2+1==1)
+	ch=(rand()%128+1);
+	else
+	ch=(rand()%128+1)*-1;
+	cout<<ch;
+ } 
+}
